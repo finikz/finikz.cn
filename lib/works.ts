db2c9b609b326ec501ae0732abceb51dedec3250
Finikz <finikz@Finikz-MacBook-Air-745.local>
2026-07-19 17:09:55 +0800

    Group West Bund translation works

--- lib/works.ts
+++ lib/works.ts
@@ -15,7 +15,7 @@ export const works: Work[] = [
   { title: "自由之水", subtitle: "朗姆酒简史", year: "2026", author: "[法] 克里斯托夫·吉塔尔", publisher: "广东人民出版社", url: "https://book.douban.com/subject/38182736/", cover: "/works/covers/rum.jpg", group: "酒神三部曲" },
   { title: "抽象艺术先驱", subtitle: "康定斯基", year: "2021", author: "西岸美术馆、蓬皮杜中心 编", publisher: "上海人民美术出版社", url: "https://book.douban.com/subject/35477045/", cover: "/works/covers/kandinsky.jpg", group: "西岸美术馆" },
   { title: "万物的声音", year: "2021", author: "西岸美术馆、蓬皮杜中心 编", publisher: "上海人民美术出版社", url: "https://book.douban.com/subject/35618278/", cover: "/works/covers/voice-of-things.jpg", group: "西岸美术馆" },
-  { title: "本源之画", subtitle: "超现实主义与东方", year: "2023", author: "张凤鸣", publisher: "上海人民出版社", url: "https://book.douban.com/subject/36552511/", cover: "/works/covers/origin.jpg", group: "其他译作" },
-  { title: "她们与抽象", year: "2022", author: "张凤鸣", publisher: "上海人民美术出版社", url: "https://book.douban.com/subject/36552512/", cover: "/works/covers/women-abstraction.jpg", group: "其他译作" },
+  { title: "本源之画", subtitle: "超现实主义与东方", year: "2023", author: "张凤鸣", publisher: "上海人民出版社", url: "https://book.douban.com/subject/36552511/", cover: "/works/covers/origin.jpg", group: "西岸美术馆" },
+  { title: "她们与抽象", year: "2022", author: "张凤鸣", publisher: "上海人民美术出版社", url: "https://book.douban.com/subject/36552512/", cover: "/works/covers/women-abstraction.jpg", group: "西岸美术馆" },
   { title: "旅行者", year: "2012", author: "[法] 阿兰·罗伯-格里耶", publisher: "湖南美术出版社", url: "https://book.douban.com/subject/6831049/", cover: "/works/covers/traveler.jpg", group: "其他译作" },
 ];
